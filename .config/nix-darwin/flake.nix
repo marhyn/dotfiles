8b0ec4b70d2be7a4823d5244eb57237c486520d2
marhyn <50330305+marhyn@users.noreply.github.com>
2025-01-04 10:31:11 +0100

    ghostty

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -24,12 +24,17 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [
           pkgs.neovim
+          pkgs.yadm
+          pkgs.fira-code-nerdfont
+          pkgs.zoxide
+          pkgs.mkalias
+          pkgs.oh-my-posh
         ];
 
       # Necessary for using flakes on this system.
@@ -46,6 +51,26 @@
               "aerospace"
           ];
       };
+
+system.activationScripts.applications.text = let
+  env = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = "/Applications";
+  };
+in
+  pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+      '';
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
